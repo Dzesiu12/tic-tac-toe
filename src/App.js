6279c9b13d4979d2213dccb237110348eaeffc69
Dzesiu12 <grzegorz_matlok@outlook.com>
2023-06-08 01:44:11 +0200

    added game logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Board from "./Board";
+import Game from "./Game";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div className="content">
-          <div className="boxBox">
-           <Board />
-          </div>
-          <div className="history">
-
-          </div>
-        </div>
+        <Game />
       </header>
     </div>
   );
